e70e318d6df29b5489c14025a735e8eee363087a
Laurens Van Keer <laurens.vankeer@colruytgroup.com>
2021-01-25 13:55:22 +0100

    add a higher-order function exercise

--- src/01-javascript/06-higher-order-functions/higher-order-functions.spec.ts
+++ src/01-javascript/06-higher-order-functions/higher-order-functions.spec.ts
@@ -1,4 +1,4 @@
-import { greaterThan } from './higher-order-functions';
+import { greaterThan, repeat } from './higher-order-functions';
 
 describe('higher order functions', () => {
   test.each`
@@ -10,5 +10,12 @@ describe('higher order functions', () => {
   ${2} | ${3} | ${true}
   `('$m is greater than $n: $greater', ({ n, m, greater }) => {
     expect(greaterThan(n)(m)).toBe(greater);
+  });
+
+  it('must repeat a function N times', () => {
+    const f: (i: number) => boolean = jest.fn(greaterThan(3));
+    const results = repeat<boolean>(7, f);
+    expect(f).toHaveBeenCalledTimes(7);
+    expect(results).toEqual([false, false, false, false, true, true, true]);
   });
 });
--- src/01-javascript/06-higher-order-functions/higher-order-functions.ts
+++ src/01-javascript/06-higher-order-functions/higher-order-functions.ts
@@ -4,3 +4,14 @@
 export function greaterThan(n: number): (m: number) => boolean {
   return (m: number) => m > n;
 }
+
+/**
+ * Executes the given function "n" times and returns the results as an array.
+ */
+export function repeat<T = any>(n: number, f: (i: number) => T): T[] {
+  const results: T[] = [];
+  for (let i = 0; i < n; i++) {
+    results.push(f(i));
+  }
+  return results;
+}
